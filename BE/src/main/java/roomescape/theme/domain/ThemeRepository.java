297567bfa951b24dd878b03d7c7b32b810a3d469
jyt6640 <jyt6640@gmail.com>
2026-05-05 16:40:59 +0900

    feat: add ThemeJdbcTemplateRepository

--- BE/src/main/java/roomescape/theme/domain/ThemeRepository.java
+++ BE/src/main/java/roomescape/theme/domain/ThemeRepository.java
@@ -2,10 +2,9 @@ package roomescape.theme.domain;
 
 import java.util.List;
 import java.util.Optional;
-import roomescape.reservation.domain.Reservation;
 
 public interface ThemeRepository {
-    Theme save(Reservation reservation);
+    Theme save(Theme theme);
     Optional<Theme> findById(Long id);
     List<Theme> findAll();
     void deleteById(Long id);
---
+++ BE/src/main/java/roomescape/theme/infrastructure/ThemeJdbcTemplateRepository.java
@@ -0,0 +1,67 @@
+package roomescape.theme.infrastructure;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import roomescape.theme.domain.Theme;
+import roomescape.theme.domain.ThemeRepository;
+
+public class ThemeJdbcTemplateRepository implements ThemeRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    public ThemeJdbcTemplateRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("theme")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    @Override
+    public Theme save(Theme theme) {
+        Map<String, Object> params = Map.of(
+                "name", theme.name(),
+                "description", theme.description(),
+                "thumbnail", theme.thumbnail()
+        );
+        Long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
+        return Theme.createWithId(id, theme.name(), theme.description(), theme.thumbnail());
+    }
+
+    @Override
+    public Optional<Theme> findById(Long id) {
+        String sql = "SELECT id, name, description, thumbnail FROM theme WHERE id = ?";
+        List<Theme> theme = jdbcTemplate.query(sql,
+                (rs, rowNum) -> Theme.createWithId(
+                    rs.getLong("id"),
+                    rs.getString("name"),
+                    rs.getString("description"),
+                    rs.getString("thumbnail")
+            ),
+            id
+        );
+        return theme.stream()
+                .findFirst();
+    }
+
+    @Override
+    public List<Theme> findAll() {
+        String sql = "SELECT id, name, description, thumbnail FROM theme";
+        return jdbcTemplate.query(sql,
+                (rs, rowNum) -> Theme.createWithId(
+                        rs.getLong("id"),
+                        rs.getString("name"),
+                        rs.getString("description"),
+                        rs.getString("thumbnail")
+                ));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        jdbcTemplate.update("DELETE FROM theme WHERE id = ?", id);
+    }
+}
