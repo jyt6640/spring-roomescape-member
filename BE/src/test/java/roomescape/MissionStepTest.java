49abcec7643a133c53ad8c5e4953905d015c5943
jyt6640 <jyt6640@gmail.com>
2026-05-05 23:54:11 +0900

    test: refactor MissionStepTest

--- BE/src/test/java/roomescape/MissionStepTest.java
+++ BE/src/test/java/roomescape/MissionStepTest.java
@@ -19,10 +19,7 @@ import roomescape.theme.presentation.ThemeController;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
-        properties = "spring.sql.init.data-locations=classpath:empty-data.sql"
-)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
